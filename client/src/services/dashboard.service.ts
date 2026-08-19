42ad894cae335c5f8fe1ed3028598c78d24dfca1
Sneha <snehagoyal711@gmail.com>
2026-08-19 17:32:25 +0530

    Update dashboard.service.ts

--- client/src/services/dashboard.service.ts
+++ client/src/services/dashboard.service.ts
@@ -1,7 +1,12 @@
-import api from "../lib/axios";
+import axios from "axios";
 import type { DashboardResponse } from "../types/dashboard";
 
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true,
+});
+
 export const getDashboard = async (): Promise<DashboardResponse> => {
-  const response = await api.get("/dashboard");
+  const response = await API.get("/dashboard");
   return response.data.data;
 };
